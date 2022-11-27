98c554d4905d24561a6251f632af1e6e2c250f75
Valerii Kurochka <78504019+Va110k@users.noreply.github.com>
2022-11-27 15:26:19 +0200

    task 1 complete

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,34 @@
-const listEl = document.querySelectorAll(".item");
-console.log("Number of categories:", listEl.length);
+function categoriesNumbers() {
+  const getCategories = document.querySelectorAll("li.item");
+  return console.log(`Number of categories: ${getCategories.length}`);
+}
 
-const navItemEl = document.querySelector("li");
+categoriesNumbers();
+
+function categoriesAll() {
+  const getCategories = document.querySelectorAll("li.item");
+  getCategories.forEach((category) => {
+    console.log(
+      `Category: ${category.querySelector("h2").textContent}
+Elements: ${category.querySelectorAll("li").length}`
+    );
+  });
+}
+
+categoriesAll();
 
 // Напиши скрипт который:
 
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// Посчитает и выведет в консоль количество категорий в ul#categories,
+// то есть элементов li.item.
+
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет
+// в консоль текст заголовка элемента (тега <h2>) и количество элементов в
+// категории (всех вложенных в него <li>).
+
 // В результате, в консоли будут выведены такие сообщения.
 
-// Number of categories: 3
+// +++Number of categories: 3
 
 // Category: Animals
 // Elements: 4
